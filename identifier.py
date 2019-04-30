14256ffa14e0ff142fbb08412cecbd4a3fbf6dca
em370 <ermi1253@colorado.edu>
2019-04-30 15:51:06 -0600

    proper output

--- identifier.py
+++ identifier.py
@@ -16,20 +16,35 @@ def sentence_from_seq(seq):
 def get_surrounding_words_pos(sequence, i):
     parts_of_speech = {}
     words = {}
+    alpha = {} #checks if its only letters
     pos_tags = sequence_to_pos_tags[sequence]
     if i > 0:
         parts_of_speech['pos_prev'] = pos_tags[i - 1][1]
         words['word_prev'] = parse_word_from_component(sequence[i - 1])
+        
     if i < len(sequence) - 1:
         parts_of_speech['pos_next'] = pos_tags[i + 1][1]
-        words['word_prev'] = parse_word_from_component(sequence[i - 1])
+        words['word_next'] = parse_word_from_component(sequence[i + 1])
+    if i>1:
+        parts_of_speech['pos_pprev'] = pos_tags[i - 2][1]
+        words['word_pprev'] = parse_word_from_component(sequence[i - 2])
+    if i < len(sequence) - 2:
+        parts_of_speech['pos_nnext'] = pos_tags[i + 2][1]
+        words['word_nnext'] = parse_word_from_component(sequence[i + 2])
+        
     return words, parts_of_speech
     
 
 def features(sequence, i):
-    yield "word=" + parse_word_from_component(sequence[i].lower())
-    if sequence[i].isupper():
-        yield "Uppercase"
+    word = parse_word_from_component(sequence[i].lower())
+    yield "word=" + word
+    yield "al=" + str(word.isalpha())
+    #if sequence[i].isupper():
+    #    yield "Case= Upper"
+    #elif sequence[i].islower():
+    #    yield "Case= Lower"
+    #else:
+    #    yield "Case= Mix"
     if sequence in sequence_to_pos_tags:
         pos_tags = sequence_to_pos_tags[sequence]
         pos = pos_tags[i][1]
@@ -49,13 +64,22 @@ def features(sequence, i):
         yield "word_next:{}".format(words['word_next'])
     if 'pos_next' in pos:
         yield "pos_next:{}".format(pos['pos_next'])
+    if 'pos_nnext' in pos:
+        yield "pos_nnext:{}".format(pos['pos_nnext'])
+    if 'word_nnext' in words:
+        yield "word_nnext:{}".format(words['word_nnext'])
+    if 'word_pprev' in words:
+        yield "word_pprev:{}".format(words['word_pprev'])
+    if 'pos_pprev' in pos:
+        yield "pos_pprev:{}".format(pos['pos_pprev'])
+
         
         
         
 import os
 from seqlearn.datasets import load_conll
 X_train, y_train, lengths_train = load_conll("train.txt", features)
-print(X_train[1])
+#print(X_train[1])
 from seqlearn.perceptron import StructuredPerceptron
 clf = StructuredPerceptron()
 clf.fit(X_train, y_train, lengths_train)
@@ -87,6 +111,7 @@ with open('output.txt', 'w') as f:
             prevNum = int(number)
         else:
             f.write("\n")
+            f.write("{}	{}	{}\n".format(number,word,tag))
             prevNum = int(number)
 
 print(bio_f_score(y_test, y_pred))
